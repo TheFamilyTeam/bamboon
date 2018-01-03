f85b9b331fee8624088fc2b430721dc797631b3e
TheFamilyTeam <layvip@hi2.in>
2018-01-03 23:56:04 +0100

    multithreading + antibot (optional)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import threading
 import time
 
 settings = {
+	'antibot':True,
 	'messages_per_second':3,
 	'token':'your_bot_api_token_here'
 }
@@ -17,6 +18,15 @@ def delete(chat, ids, bot):
 			print("[i] " + str(chat) + " -> deleted " + str(x))
 	except Exception as e:
 		print("[!] Delete : " + str(e))
+
+def antibot(msg):
+	if 'new_chat_members' in msg:
+		new = msg['new_chat_members']
+		for x in new:
+			if x['is_bot'] == True:
+				chat = msg['chat']['id']
+				bot.kickChatMember(chat, x['id'])
+				print("[i] " + str(chat) + " -> banned bot " + str(x['id']))
 
 def handle(msg):
 	try:
@@ -42,16 +52,32 @@ def handle(msg):
 				print("[i] " + str(chat) + " -> banned " + str(user))
 				t = threading.Thread(target=delete, args=(chat, wd, bot,),)
 				t.start()
+
+				
 	except Exception as e:
 		print("[!] Bamboon : " + str(e))
+
+def magic(update):
+	try:
+		t1 = threading.Thread(target=handle, args=(update,),)
+		t1.start()
+
+		if settings['antibot']:
+			t2 = threading.Thread(target=antibot, args=(update,),)
+			t2.start()
+	except Exception as e:
+		print("[!] Magic : " + str(e))
+
+
 
 if __name__ == '__main__':
 	print('\n'.join([" ____                  _                        ", "|  _ \                | |                       ", "| |_) | __ _ _ __ ___ | |__   ___   ___  _ __   ", "|  _ < / _` | '_ ` _ \| '_ \ / _ \ / _ \| '_ \  ", "| |_) | (_| | | | | | | |_) | (_) | (_) | | | | ", "|____/ \__,_|_| |_| |_|_.__/ \___/ \___/|_| |_| "]))
 	print('          - created by TheFamilyTeam -')
 	print("\n[i] Token: " + settings['token'])
+	print("[i] Antibot: " + ('Yes' if settings['antibot'] else 'No'))
 	print("====================")
 	try:
-		bot.message_loop(handle)
+		bot.message_loop(magic)
 	except Exception as e:
 		print("[!] Loop : " + str(e))
 
