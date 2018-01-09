831b5f151b4d5ec6468f4c972f7db965d1931938
TheFamilyTeam <layvip@hi2.in>
2018-01-09 16:45:17 +0100

    edit bug fixed + added private mode

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ import time
 settings = {
 	'antibot':True,
 	'messages_per_second':3,
-	'token':'your_bot_api_token_here'
+	'token':'529885669:AAEpOEAICGLAUb_6h53XeuB-eegkW4QQGHY',
+	'private_settings':{
+		'private':True,
+		'groups':['-1001379719554'],
+	}
 }
 
 bot = telepot.Bot(settings['token'])
 antiflood = {}
+
+def private_setting():
+	return settings['private_settings']['private'] == True
+
+def private_allow(group):
+	return group in settings['private_settings']['groups']
 
 def delete(chat, ids, bot):
 	try:
@@ -20,13 +30,14 @@ def delete(chat, ids, bot):
 		print("[!] Delete : " + str(e))
 
 def antibot(msg):
-	if 'new_chat_members' in msg:
-		new = msg['new_chat_members']
-		for x in new:
-			if x['is_bot'] == True:
-				chat = msg['chat']['id']
-				bot.kickChatMember(chat, x['id'])
-				print("[i] " + str(chat) + " -> banned bot " + str(x['id']))
+	if settings['private_settings']['private'] and msg['chat']['id'] in settings['private_settings']['groups']:
+		if 'new_chat_members' in msg:
+			new = msg['new_chat_members']
+			for x in new:
+				if x['is_bot'] == True:
+					chat = msg['chat']['id']
+					bot.kickChatMember(chat, x['id'])
+					print("[i] " + str(chat) + " -> banned bot " + str(x['id']))
 
 def handle(msg):
 	try:
@@ -34,37 +45,44 @@ def handle(msg):
 		user = msg['from']['id']
 		date = msg['date']
 		msgid = msg['message_id']
-		if  msg['chat']['type'] != 'private':
-			if not chat in antiflood:
-				antiflood[chat] = {}
+		if not 'edit_date' in msg:
+			if msg['chat']['type'] != 'private':
+				if not chat in antiflood:
+					antiflood[chat] = {}
 		
-			if not user in antiflood[chat]:
-				antiflood[chat][user] = {}
+				if not user in antiflood[chat]:
+					antiflood[chat][user] = {}
 
-			if not date in antiflood[chat][user]:
-				antiflood[chat][user][date] = []
+				if not date in antiflood[chat][user]:
+					antiflood[chat][user][date] = []
 
-			wd = antiflood[chat][user][date]
-			wd.append(msgid)
+				wd = antiflood[chat][user][date]
+				wd.append(msgid)
 
-			if len(wd) >= settings['messages_per_second']:
-				bot.kickChatMember(chat, user)
-				print("[i] " + str(chat) + " -> banned " + str(user))
-				t = threading.Thread(target=delete, args=(chat, wd, bot,),)
-				t.start()
-
-				
+				if len(wd) >= settings['messages_per_second']:
+					bot.kickChatMember(chat, user)
+					print("[i] " + str(chat) + " -> banned " + str(user))
+					t = threading.Thread(target=delete, args=(chat, wd, bot,),)
+					t.start()
 	except Exception as e:
 		print("[!] Bamboon : " + str(e))
 
 def magic(update):
 	try:
 		t1 = threading.Thread(target=handle, args=(update,),)
-		t1.start()
-
-		if settings['antibot']:
-			t2 = threading.Thread(target=antibot, args=(update,),)
-			t2.start()
+		t2 = threading.Thread(target=antibot, args=(update,),)
+		if private_setting():
+			if private_allow(str(update['chat']['id'])):
+				t1.start()
+				if settings['antibot']:
+					t2.start()
+			else:
+				print("[i] Unallowed group ("+str(update['chat']['id'])+"), leaving... ")
+				bot.leaveChat(update['chat']['id'])
+		else:
+			t1.start()
+			if settings['antibot']:
+				t2.start()
 	except Exception as e:
 		print("[!] Magic : " + str(e))
 
@@ -75,6 +93,7 @@ if __name__ == '__main__':
 	print('          - created by TheFamilyTeam -')
 	print("\n[i] Token: " + settings['token'])
 	print("[i] Antibot: " + ('Yes' if settings['antibot'] else 'No'))
+	print("[i] Private Mode: " + ('Yes' if private_setting() else 'No') + " (" + (', '.join(settings['private_settings']['groups']) if settings['private_settings']['groups'] else "No groups") + ")")
 	print("====================")
 	try:
 		bot.message_loop(magic)
